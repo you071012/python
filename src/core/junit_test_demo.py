76d0b699886bbdf92c11de59632ac9f50c7fec00
youjia <you071012@qq.com>
2020-05-25 17:27:09 +0800

    commit

--- src/core/junit_test_demo.py
+++ src/core/junit_test_demo.py
@@ -1,14 +1,30 @@
 import unittest
 
-from src.core import test_class_demo
+# from src.core import test_class_demo
 
+class Calc():
+    def __init__(self, x, y):
+        self.x = x;
+        self.y = y;
+
+
+    def add(self):
+        return self.x + self.y
+
+    def sub(self):
+        return self.x - self.y
+
+
+"""
+    测试类必须继承 unittest.TestCase 类
+"""
 class TestJunit(unittest.TestCase):
     """
         运行测试之前执行的方法
     """
     def setUp(self) -> None:
         print("开始准备初始化calc类......")
-        self.calc = test_class_demo.Calc(10, 5)
+        self.calc = Calc(10, 5)
 
     """
         运行测试之后执行的方法
